15af9e00fea9d00e3497c666ee6acd81135ed667
Daniel Vovk <33673094+borisvovk1987@users.noreply.github.com>
2018-06-13 15:16:13 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -5,10 +5,11 @@ TestCase:: TestCase(string testName,ostream &out ):totalTests(0),passedTests(0),
 {
 }
 
-void TestCase::print()
+TestCase& TestCase::print()
 {
 
     string  str=testName+" "+to_string(failedTests)+" failed, "+to_string(passedTests)+" passed, "+to_string(totalTests)+" total.\n";
     std::cout<<str;
+    return *this;
 
 }
